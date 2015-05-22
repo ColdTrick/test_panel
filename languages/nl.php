e06fb05e0c406277ed01a44797b2494ae94a52fd
Jerome Bakker <jeabakker@coldtrick.com>
2015-05-22 12:27:23 +0200

    added: Dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file was created by Translation Editor v4.0
+ * On 2015-05-22 12:25
+ */
+
+return array (
+  'test_panel' => 'Test panel',
+  'test_panel:settings:info' => 'Zonder aanvullende configuratie wordt de toegang tot de site gelimiteerd tot site beheerders.
+Door het toevoegen van een of meerdere groep guids kun je de toegang tot de site uitbreiden met de opgegeven groepen.
+Alle leden van e geconfigureerde groepen kunnen de site benaderen.
+Als je geen toegang hebt krijg je een eenvoudige pagina te zien met de geconfigureerde titel en bericht.',
+  'test_panel:settings:group_guids' => 'Groep guids (komma gescheiden)',
+  'test_panel:settings:limit_notifications' => 'Limiteer uitgaande e-mail notificaties tot test panel leden',
+  'test_panel:settings:limit_notifications:description' => 'Als dit is ingeschakeld zullen niet test panel leden GEEN e-mail ontvangen welke door het Elgg notificatie systeem worden verzonden (bijv. nieuwe blog of uitnodigingen).',
+  'test_panel:settings:message' => 'Bericht voor niet test panel leden',
+  'test_panel:settings:message_title' => 'Titel',
+  'test_panel:settings:message_content' => 'Bericht',
+);
